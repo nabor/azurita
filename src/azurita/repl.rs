0bf1be23f88a4b93b7d3193b1df3cc2d8a57d151
Nabor Erices <nabor@erices.com>
2017-06-14 18:59:17 -0400

    Changed to rustyline

--- src/azurita/repl.rs
+++ src/azurita/repl.rs
@@ -1,23 +1,39 @@
-extern crate linenoise;
+extern crate rustyline;
+
+use self::rustyline::Editor;
+use self::rustyline::error::ReadlineError;
 
 use azurita::parser;
 
 pub fn run() {
     println!("Azurita 0.0.1  (Press Ctlr+c to exit)");
+    let mut rl = Editor::<()>::new();
+    if let Err(_) = rl.load_history("history.txt") {
+        println!("No previous history.");
+    }
     loop {
-        let val = linenoise::input("(°)> ");
+        let val = rl.readline("(°)> ");
         match val {
-            Some(input) => {
-                linenoise::history_add(input.as_str());
-                if input.as_str() == "clear" {
-                  linenoise::clear_screen();
-                }
+            Ok(input) => {
+                rl.add_history_entry(&input);
                 match parser::eval(&input) {
                     Err(msg) => println!("Error: {}", msg),
                     _ => {},
                 }
             },
-            None => { break },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
         }
     }
+    rl.save_history("history.txt").unwrap();
 }
